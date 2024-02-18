3663d851962ce7dcff4fcb5c19327a22d2ef9d7c
sandhya-lagudu <lagudusandhya27@gmail.com>
2024-02-18 23:34:35 +0530

    this is forntend and backend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import problemTable from "./components/problemTable";
+
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-800 text-white text-4xl py-6 px-2 text-center">List of Problems</h1>
+      <problemTable />
+    </>
+  )
+}
+
+export default App
